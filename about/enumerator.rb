5935086f0028330c25280c1a00ecb935ad4bc649
B Kleinen <kleinen@infrastructure.de>
2013-11-29 12:27:10 +0100

    reviewed enumeration

--- about/enumerator.rb
+++ about/enumerator.rb
@@ -1,13 +1,13 @@
 describe "enumerators" do
 
-	it "map returns math iteration array" do
+	it "map applies a given block to each element" do
 		result = (1..4).map do |element|
 			element+2
 		end
 		expect(result).to eq [3,4,5,6]
 	end
 
-	it "inject sums an array" do
+	it "inject combines all elements by applying a block " do
 		result = (4..8).inject {|sum, n | sum + n}
 		expect(result). to eq 30
 	end
