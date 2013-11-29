cd230e692324e51609fbe848d8431d47aecc2da9
B Kleinen <kleinen@infrastructure.de>
2013-11-29 12:33:06 +0100

    reviewed regexes

--- about/regex.rb
+++ about/regex.rb
@@ -1,6 +1,14 @@
+
+
 # These are tests for the use of regex in ruby
 
-describe "A Ruby class" do
+# great page to try out ruby regexes:
+# http://rubular.com/
+
+describe "Regular Expressions" do
+	# hm. very nice examples, but it would have been better if you'd
+	# split it up with various examples, eg.
+	# it "can check if there are numbers in the string"
 	it "tests regex" do
 		s = "ichBinEinString1234"
 		#Let's see if there are numbers in the String
@@ -29,14 +37,14 @@ describe "A Ruby class" do
 		s2 = "I am a text with some vowels in it"
 		matchVowels = s2.match(/[aeiouAEIOU]/) #Returns an match-object, that
 		#can be addressed like an array, but only contains the first match!
-		expect(matchVowels[0]).to eq "I"		
+		expect(matchVowels[0]).to eq "I"
 
 		r = /(\w*).(\w*)/ #This regex finds character splitted by a dot
 		s3 = "IchBin.EinText" #This is a text divided by a dot
 		matchR = s3.match(r) #Match the string and get the matchR-match-object
 		expect(matchR[0]).to eq "IchBin.EinText" #The first contains the whole matched string (if it matched)
 		expect(matchR[1]).to eq "IchBin" #The first entry contains the whole right part
-		expect(matchR[2]).to eq "EinText" #The second entry contains the whole left part		
+		expect(matchR[2]).to eq "EinText" #The second entry contains the whole left part
 
 		#I am not sure if there is an Apple or an Orange in the string
 		s4 = "It's an Apple"
