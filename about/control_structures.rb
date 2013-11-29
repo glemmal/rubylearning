483ec47d46bf4fe29ad6f3204a65816594562345
B Kleinen <kleinen@infrastructure.de>
2013-11-29 13:50:56 +0100

    split examples and replaced tabs with spaces

--- about/control_structures.rb
+++ about/control_structures.rb
@@ -1,65 +1,72 @@
 describe "Testing the Ruby control structures;" do
-	it "are if/unless branches working?" do
-		
-		a = 2
-		b = 0
-		c = 5
-		
-		if a == 2
-			b = true
-		else
-			b = false
-		end
-		
-		unless c == 7
-			c = 10
-		end
-		
-		expect(b).to be_true
-		expect(c).to eq(10)
-	end
-	
-	it "are for while/until working?" do
-		
-		a = 1
-		b = 0
-		
-		while a < 10
-			a = a + 1
-		end
-		
-		until b == 10
-			b = b + 1
-		end
-		
-		expect(a).to eq(10)
-		expect(b).to eq(10)
-	end
-	
-	it "tests if for loops are working" do
-		
-		a = 0
-		b = 10
-		
-		for a in (0..b)
-			# do stuff
-		end
-		
-		expect(a).to eq(10)
-		
-	end
-	
-	it "tests if case is working" do
-		
-		a = 2
-		b = 0
-		
-		case
-			when a < 2 then b = 1
-			when a == 2 then b = 2
-			when a > 2 then b = 3
-		end
-		
-		expect(b).to eq(2)
-	end
+  it "are if branches working?" do
+
+    a = 2
+    b = 0
+
+    if a == 2
+      b = true
+    else
+      b = false
+    end
+
+    expect(b).to be_true
+  end
+
+  it "are unless branches working?" do
+    c = 5
+
+    unless c == 7
+      c = 10
+    end
+    expect(c).to eq(10)
+  end
+
+  it "is for while working?" do
+
+    a = 1
+
+    while a < 10
+      a = a + 1
+    end
+    expect(a).to eq(10)
+  end
+
+  it "is until working?" do
+
+    b = 0
+    until b == 10
+      b = b + 1
+    end
+
+
+    expect(b).to eq(10)
+  end
+
+  it "tests if for loops are working" do
+
+    a = 0
+    b = 10
+
+    for a in (0..b)
+      # do stuff
+    end
+
+    expect(a).to eq(10)
+
+  end
+
+  it "tests if case is working" do
+
+    a = 2
+    b = 0
+
+    case
+      when a < 2 then b = 1
+      when a == 2 then b = 2
+      when a > 2 then b = 3
+    end
+
+    expect(b).to eq(2)
+  end
 end
