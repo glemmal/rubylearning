7c8a6ded4f888ce38c6b4ebf29d4928708933450
Tim E <tim.eulitz@gmail.com>
2013-11-29 11:15:22 +0100

    Added upto and comments

--- about/eachTo.rb
+++ about/eachTo.rb
@@ -1,17 +1,30 @@
-describe "A Ruby Class" do
-	it "beschreibung" do
-	testArray = ["Test1", "Test2", "Test3"]
-	i = 0
-	testArray.each {
-		|testString|
-		if i == 0
-			expect(testString=="Test1")
-		elsif i == 1
-			expect(testString == "Test2")
-		elsif i == 2
-			expect(testString == "Test3")
-		end
-		i = i+1
+describe "Loops" do
+	it "Test01" do
+		testArray = ["Test1", "Test2", "Test3"] #Creating an array with three strings
+		i = 0
+		
+		 #iterating through string array (in Java this would be "for (String testString : testArray) {...}")
+		 #current element of the iteration ("Test1" on first loop, "Test2" on second loop, "Test3" on third loop...)
+		 #Important: |testString| has to be in the same line as "testArray.each {"!
+		testArray.each 	{ |testString| 
+			if i == 0
+				expect(testString=="Test1")
+			elsif i == 1
+				expect(testString == "Test2")
+			elsif i == 2
+				expect(testString == "Test3")
+			end
+			i = i+1
+		}
+	end
+	
+	it "Test02" do
+	#iterating from one value to another (in Java this would be for (int i = 1, i <= 10, i++)
+	
+	sum = 0
+	1.upto(10) 	{ |i|
+		sum += i
 	}
-end
+	expect(sum == 55)
+	end
 end
