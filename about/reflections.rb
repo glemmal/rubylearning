7d7f8edba14483d7569ad254411636de86106415
Nadine Kost <kost.nadine@gmail.com>
2013-11-29 11:23:46 +0100

    all tests implemented

--- about/reflections.rb
+++ about/reflections.rb
@@ -1,17 +1,24 @@
-#reflection.rb
-#get method names, find out 
-#if an object responds to a method, 
-#send a message to an object 
-#where you have the method 
-#name in a string
 describe "Test" do
-it "kryptische worte" do
-	class Test
-		def sayhello
-			h = "hello"
-		end
-	end
+it "object responds to method" do
+class Test
+def sayhello
+h = "hello"
+end
+end
 	test = Test.new
-	expect(test.sayhello).to eq "hello"
+	expect(test.respond_to?("sayhello")).to eq true
 end
+
+it "send method name as string" do
+	test = Test.new
+	expect(test.send("sayhello")). to eq "hello"
 end
+
+it "object includes method" do
+	test = Test.new
+	expect(test.methods.include?(:sayhello)). to eq true
+end
+
+
+end
+
